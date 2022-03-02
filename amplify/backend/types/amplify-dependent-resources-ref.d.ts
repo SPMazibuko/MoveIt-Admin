baaadcee082be0104b10924c17d8573b9bd8b174
thulaninhla <87076557+thulaninhla@users.noreply.github.com>
2022-03-02 12:58:35 +0200

    amplify added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "moveitapp4cea0d19": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
