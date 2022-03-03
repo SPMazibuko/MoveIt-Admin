109382e63dc094de38c7e720457aba051847f05d
R.S. Maodi <silasmaodi@gmail.com>
2022-03-03 16:55:02 +0200

    New Amplify Backend added (with Auth, Api, Storage..)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,41 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "moveitappf4924080": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "moveitapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "S3Trigger2026952f": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "mvi01": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "analytics": {
+        "moveitapp": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
